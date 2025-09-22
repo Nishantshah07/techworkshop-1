7d897625501f74872a5c032e246f40c157c80a9e
Nishant Shah <nishantshah0617@gmail.com>
2025-09-22 19:39:19 -0400

    added github workflow

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from "next";
+import type {NextConfig} from "next";
 
+const repo = "techworkshop-1";
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+    output: "export",
+    basePath: `/${repo}`,
+    assetPrefix: `/${repo}`,
+}
 
 export default nextConfig;
